bcdb74d5b9d386d827916a98b947a055133e0577
Roslyn Saplicki <j.saplicki@gmail.com>
2018-12-30 23:35:15 -0500

    adds styling to the widget

--- main.js
+++ main.js
@@ -9,37 +9,38 @@ var request = new XMLHttpRequest();
 request.open('GET', 'https://api.taboola.com/1.2/json/apitestaccount/recommendations.get?app.type=web&app.apikey=7be65fc78e52c11727793f68b06d782cff9ede3c&source.id=%2Fdigiday-publishing-summit%2F&source.url=https%3A%2F%2Fblog.taboola.com%2Fdigiday-publishing-summit%2F&source.type=text&placement.organic-type=mix&placement.visible=true&placement.available=true&placement.rec-count=6&placement.name=Below%20Article%20Thumbnails&placement.thumbnail.width=640&placement.thumbnail.height=480&user.session=init', true);
 request.onload = function () {
 
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
-    data.list.forEach(advertisement => {
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
-      card.setAttribute("style", `width: ${advertisement.thumbnail[0].width}; height: ${advertisement.thumbnail[0].height}`);
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response);
+    if (request.status >= 200 && request.status < 400) {
+        data.list.forEach(advertisement => {
+            const adCard = document.createElement('div');
+            adCard.setAttribute('class', 'adCard');
+            // adCard.setAttribute("style", `width: ${advertisement.thumbnail[0].width}; height: ${advertisement.thumbnail[0].height}`);
 
-      const h1 = document.createElement('h1');
-      h1.textContent = advertisement.name;
-      const img = document.createElement('img');
-      const a = document.createElement('a');
-      img.setAttribute("src", advertisement.thumbnail[0].url);
-      a.setAttribute("href", advertisement.url);
-      // make it open in new tab
-      a.appendChild(img);
-      
-      const p = document.createElement('p');
-      advertisement.name = advertisement.branding.substring(0, 300);
-      p.textContent = `${advertisement.branding}`;
+            const h4 = document.createElement('h4');
+            h4.textContent = advertisement.name;
+            const img = document.createElement('img');
+            const a = document.createElement('a');
+            img.setAttribute("src", advertisement.thumbnail[0].url);
+            a.setAttribute("href", advertisement.url);
+            a.setAttribute("target", '_blank');
+            // make it open in new tab
+            a.appendChild(img);
 
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
-      card.appendChild(a);
-    });
-  } else {
-    const errorMessage = document.createElement('marquee');
-    errorMessage.textContent = `Page Error, Refresh`;
-    app.appendChild(errorMessage);
-  }
+            const p = document.createElement('p');
+            advertisement.name = advertisement.branding.substring(0, 300);
+            p.textContent = `${advertisement.branding}`;
+
+            container.appendChild(adCard);
+            adCard.appendChild(a);
+            adCard.appendChild(h4);
+            adCard.appendChild(p);
+        });
+    } else {
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = `Page Error, Refresh`;
+        app.appendChild(errorMessage);
+    }
 }
 
 request.send();
